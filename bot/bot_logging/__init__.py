8ae96a8ba8245fbd5cf23cd3f80da1be2875dc78
perite <nikita.krementar@gmail.com>
2024-05-04 11:30:41 +0200

    improving logging of creating a logger

--- bot/bot_logging/__init__.py
+++ bot/bot_logging/__init__.py
@@ -4,19 +4,25 @@ import confg
 bot_logger = logging.getLogger('bot_logger')
 if not bot_logger.handlers:
     _bot_handler = logging.FileHandler(confg.BOT_LOG_PATH)
-    _bot_formatter = logging.Formatter('%(levelname)s: %(asctime)s - %(message)s')
+    _bot_formatter = logging.Formatter(
+        '%(levelname)s: %(asctime)s - %(message)s')
     _bot_handler.setFormatter(_bot_formatter)
     bot_logger.addHandler(_bot_handler)
     bot_logger.setLevel(logging.INFO)
+
     print("Bot logger created")
-    bot_logger.info('Bot logger created')
+    logging.info("Bot logger created")
+    bot_logger.info('Bot logger created | bot_logger')
 
 bot_error_logger = logging.getLogger('bot_error_logger')
 if not bot_error_logger.handlers:
     _bot_error_handler = logging.FileHandler(confg.BOT_ERROR_LOG_PATH)
-    _bot_error_formatter = logging.Formatter('%(levelname)s: %(asctime)s - %(message)s')
+    _bot_error_formatter = logging.Formatter(
+        '%(levelname)s: %(asctime)s - %(message)s | %(filename)s - func: %(funcName)s - %(lineno)d')
     _bot_error_handler.setFormatter(_bot_error_formatter)
     bot_error_logger.addHandler(_bot_error_handler)
-    bot_error_logger.setLevel(logging.ERROR)
+    bot_error_logger.setLevel(logging.INFO)
+
     print("Bot error logger created")
-    bot_error_logger.info("Bot error logger created")
+    logging.info("Bot error logger created")
+    bot_error_logger.info("Bot error logger created | bot_error_logger")
--- web_app/web_app_logging/__init__.py
+++ web_app/web_app_logging/__init__.py
@@ -9,10 +9,19 @@ if not web_app_logger.handlers:
     web_app_logger.addHandler(_web_app_handler)
     web_app_logger.setLevel(logging.INFO)
 
+    print("web app logger initialized")
+    logging.info("web app logger initialized")
+    web_app_logger.info('web app logger initialized | web_app_logger')
+
 web_app_error_logger = logging.getLogger('web_app_error_logger')
 if not web_app_error_logger.handlers:
     _web_app_error_handler = logging.FileHandler(confg.WEB_APP_ERROR_LOG_PATH)
-    _web_app_error_formatter = logging.Formatter('%(levelname)s: %(asctime)s - %(message)s')
+    _web_app_error_formatter = logging.Formatter(
+        '%(levelname)s: %(asctime)s - %(message)s | %(filename)s - func: %(funcName)s - %(lineno)d')
     _web_app_error_handler.setFormatter(_web_app_error_formatter)
     web_app_error_logger.addHandler(_web_app_error_handler)
     web_app_error_logger.setLevel(logging.INFO)
+
+    print("web app error logger initialized")
+    logging.info("web app error logger initialized")
+    web_app_error_logger.info('web app error logger initialized | web_app_error_logger')
